81fe7b629b5d9ab30145cf93c9c7c0a3b30e3cbe
Daniil Baykalov <felix.trof@gmail.com>
2024-05-02 00:18:13 +0300

    sh script test update 4

--- install.sh
+++ install.sh
@@ -3,6 +3,18 @@ cd ~
 yes | sudo apt update
 yes | sudo apt upgrade
 
+yes | sudo apt-get update
+yes | sudo apt-get install ca-certificates curl
+yes | sudo install -m 0755 -d /etc/apt/keyrings
+yes | sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+yes | sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+yes | sudo apt-get update
+
 yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 yes | sudo service docker start
 
