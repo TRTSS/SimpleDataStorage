8adb8898138fe117c633397bdcf2acf1c9fb574d
Daniil Baykalov <felix.trof@gmail.com>
2024-05-02 00:14:37 +0300

    sh script test update 3

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
 echo "STARTING DEPLOY SDS"
 cd ~
-git clone https://github.com/TRTSS/SimpleDataStorage.git
+yes | sudo apt update
+yes | sudo apt upgrade
+
+yes | sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+yes | sudo service docker start
+
+yes | git clone https://github.com/TRTSS/SimpleDataStorage.git
+cd SimpleDataStorage
+docker build -t sds .
+docker run -d --name SimpleDataStorage -p 80:80 sds
+
 echo "DONE"
